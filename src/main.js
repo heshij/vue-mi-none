640c0ceef2fca00d943d28c97462fbea81b2d165
heshij <1634943446@qq.com>
2020-10-19 17:04:37 +0800

    init

--- src/main.js
+++ src/main.js
@@ -3,6 +3,24 @@ import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
 import store from './store'
+import 'amfe-flexible'
+import FastClick from 'fastclick'
+import 'normalize.css/normalize.css'
+
+/**
+ * If you don't want to use mock-server
+ * you want to use MockJs for mock api
+ * you can execute: mockXHR()
+ *
+ * Currently MockJs will be used in the production environment,
+ * please remove it before going online ! ! !
+ */
+if (process.env.NODE_ENV === 'production') {
+  const { mockXHR } = require('../mock')
+  mockXHR()
+}
+
+FastClick.attach(document.body)
 
 Vue.config.productionTip = false
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-button type="info" size="small" @click="showToast">按钮1</van-button>
+    <ul>
+      <li v-for="(item, index) in datalists" :key="index">{{item}}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { Button, Toast } from 'vant'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    [Button.name]: Button
+  },
+  data () {
+    return {
+      datalists: ['test1', 'test2', 'test3', 'test4', 'test5', 'test6']
+    }
+  },
+  methods: {
+    showToast () {
+      Toast('点击')
+    }
   }
 }
 </script>
+<style lang="less" scoped>
+</style>
